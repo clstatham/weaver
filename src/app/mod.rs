5063f307635e22769b606d3347c868c4c8d94718
clstatham <58794204+clstatham@users.noreply.github.com>
2023-11-15 10:43:02 -0600

    app abstraction

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,1 +1,92 @@
+use pixels::{Pixels, PixelsBuilder, SurfaceTexture};
+use raqote::{DrawTarget, SolidSource};
+use winit::{
+    event::Event,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+use winit_input_helper::WinitInputHelper;
 
+pub struct App {
+    event_loop: EventLoop<()>,
+    input: WinitInputHelper,
+    window: Window,
+    pixels: Pixels,
+    dt: DrawTarget,
+    renderer: crate::renderer::Renderer,
+}
+
+impl App {
+    pub fn new(window_size: (u32, u32), window_title: &str) -> App {
+        let event_loop = EventLoop::new();
+        let input = WinitInputHelper::new();
+
+        let window = {
+            WindowBuilder::new()
+                .with_title(window_title)
+                .with_inner_size(winit::dpi::LogicalSize::new(window_size.0, window_size.1))
+                .with_resizable(false)
+                .build(&event_loop)
+                .unwrap()
+        };
+
+        let window_size = window.inner_size();
+        let pixels = {
+            let surface_texture =
+                SurfaceTexture::new(window_size.width, window_size.height, &window);
+            PixelsBuilder::new(window_size.width, window_size.height, surface_texture)
+                .texture_format(pixels::wgpu::TextureFormat::Bgra8UnormSrgb) // compat with raqote's DrawTarget
+                .build()
+                .unwrap()
+        };
+
+        let dt = DrawTarget::new(window_size.width as i32, window_size.height as i32);
+
+        let renderer = crate::renderer::Renderer::new();
+
+        App {
+            event_loop,
+            input,
+            window,
+            pixels,
+            dt,
+            renderer,
+        }
+    }
+
+    pub fn run(mut self) -> anyhow::Result<()> {
+        self.event_loop.run(move |event, _, control_flow| {
+            if self.input.update(&event) {
+                // ...
+                if self.input.close_requested() {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+
+            if let Event::RedrawRequested(_) = event {
+                self.dt.clear(SolidSource {
+                    r: 50,
+                    g: 50,
+                    b: 50,
+                    a: 255,
+                });
+
+                if let Err(err) = self.renderer.render(&mut self.dt) {
+                    log::error!("renderer.render() failed: {}", err);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+
+                self.pixels
+                    .frame_mut()
+                    .copy_from_slice(self.dt.get_data_u8());
+                if let Err(err) = self.pixels.render() {
+                    log::error!("pixels.render() failed: {}", err);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,7 @@
-use pixels::{wgpu::TextureFormat, PixelsBuilder, SurfaceTexture};
-use raqote::{DrawTarget, SolidSource};
-use winit::{
-    dpi::LogicalSize,
-    event::Event,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit_input_helper::WinitInputHelper;
-
 pub mod app;
 pub mod renderer;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
-
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-
-    let window = {
-        WindowBuilder::new()
-            .with_title("Weaver")
-            .with_inner_size(LogicalSize::new(800, 600))
-            .with_resizable(false)
-            .build(&event_loop)?
-    };
-    let window_size = window.inner_size();
-    let mut pixels = {
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        PixelsBuilder::new(window_size.width, window_size.height, surface_texture)
-            .texture_format(TextureFormat::Bgra8UnormSrgb) // compat with raqote's DrawTarget
-            .build()?
-    };
-
-    let mut dt = DrawTarget::new(window_size.width as i32, window_size.height as i32);
-
-    let renderer = renderer::Renderer::new();
-
-    event_loop.run(move |event, _, control_flow| {
-        if input.update(&event) {
-            // ...
-            if input.close_requested() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-
-        if let Event::RedrawRequested(_) = event {
-            dt.clear(SolidSource {
-                r: 50,
-                g: 50,
-                b: 50,
-                a: 255,
-            });
-
-            if let Err(err) = renderer.render(&mut dt) {
-                log::error!("renderer.render() failed: {}", err);
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-
-            pixels.frame_mut().copy_from_slice(dt.get_data_u8());
-            if let Err(err) = pixels.render() {
-                log::error!("pixels.render() failed: {}", err);
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-
-        window.request_redraw();
-    });
+    app::App::new((800, 600), "Weaver").run()
 }
