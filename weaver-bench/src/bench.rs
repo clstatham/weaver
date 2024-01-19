a1d0d46be5c3c9a89cd194451c9927a1c507fe76
clstatham <58794204+clstatham@users.noreply.github.com>
2024-01-19 12:09:47 -0600

    remove a bench

--- weaver-bench/src/bench.rs
+++ weaver-bench/src/bench.rs
@@ -63,42 +63,5 @@ pub fn weaver_query_iter_many_entities(c: &mut Criterion) {
     group.finish();
 }
 
-pub fn weaver_query_par_iter_many_entities(c: &mut Criterion) {
-    use weaver_ecs::*;
-
-    let plot_config =
-        criterion::PlotConfiguration::default().summary_scale(criterion::AxisScale::Logarithmic);
-
-    let mut group = c.benchmark_group("weaver_query_par_iter_many_entities");
-    group.plot_config(plot_config);
-    group.sampling_mode(criterion::SamplingMode::Linear);
-
-    for n in ENTITY_COUNTS.iter() {
-        group.throughput(criterion::Throughput::Elements(*n as u64));
-        group.bench_with_input(criterion::BenchmarkId::from_parameter(n), n, |b, n| {
-            let mut world = World::new();
-
-            for _ in 0..*n {
-                world.spawn(A {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                });
-            }
-
-            b.iter(|| {
-                let q = world.query::<&A>();
-                black_box(q.par_iter().count());
-            })
-        });
-    }
-
-    group.finish();
-}
-
-criterion_group!(
-    benches,
-    weaver_query_iter_many_entities,
-    weaver_query_par_iter_many_entities,
-);
+criterion_group!(benches, weaver_query_iter_many_entities,);
 criterion_main!(benches);
