e553130f53634354c34d33f95696c84fc171ddcf
clstatham <58794204+clstatham@users.noreply.github.com>
2024-01-26 07:33:01 -0600

    syntax highlighting for script editor

--- weaver-editor/src/ui/mod.rs
+++ weaver-editor/src/ui/mod.rs
@@ -2,6 +2,7 @@ use egui::TextEdit;
 use weaver::prelude::{weaver_core::scripts::Scripts, *};
 
 use crate::state::EditorState;
+pub mod syntax_highlighting;
 
 #[system(UiMain)]
 pub fn ui_main(ctx: Res<EguiContext>, state: Res<EditorState>, fps_display: ResMut<FpsDisplay>) {
@@ -14,7 +15,7 @@ pub fn ui_main(ctx: Res<EguiContext>, state: Res<EditorState>, fps_display: ResM
     });
 }
 
-#[system(ReloadScripts)]
+#[system(ScriptUpdate)]
 pub fn reload_scripts(ctx: Res<EguiContext>, scripts: Res<Scripts>) {
     ctx.draw_if_ready(|ctx| {
         egui::Window::new("Reload scripts").show(ctx, |ui| {
@@ -24,18 +25,26 @@ pub fn reload_scripts(ctx: Res<EguiContext>, scripts: Res<Scripts>) {
         });
 
         for mut script in scripts.script_iter_mut() {
+            let mut layouter = |ui: &egui::Ui, string: &str, wrap_width| {
+                let mut layout_job = syntax_highlighting::highlight(ui.ctx(), &string);
+                layout_job.wrap.max_width = wrap_width;
+                ui.fonts(|f| f.layout_job(layout_job))
+            };
             egui::Window::new(&script.name)
                 .max_height(400.0)
                 .max_width(800.0)
+                .scroll2([true, true])
                 .show(ctx, |ui| {
                     if ui.button("Save").clicked() {
                         script.save().unwrap();
                     }
-                    TextEdit::multiline(&mut script.content)
-                        .code_editor()
-                        .desired_width(800.0)
-                        .desired_rows(20)
-                        .show(ui);
+                    ui.add(
+                        TextEdit::multiline(&mut script.content)
+                            .code_editor()
+                            .desired_width(f32::INFINITY)
+                            .desired_rows(20)
+                            .layouter(&mut layouter),
+                    );
                 });
         }
     });
