4edafbb04bd5db76c843c4059155fde3326dc02e
clstatham <58794204+clstatham@users.noreply.github.com>
2024-06-05 14:51:42 -0500

    repository restructuring; moved weaver crates into "crates" folder

---
+++ crates/weaver-proc-macro/target/debug/deps/weaver_proc_macro-b65c1ac59e89580a.d
@@ -0,0 +1,9 @@
+C:\Users\slash\code\weaver\weaver-proc-macro\target\debug\deps\weaver_proc_macro-b65c1ac59e89580a.rmeta: src\lib.rs Cargo.toml
+
+C:\Users\slash\code\weaver\weaver-proc-macro\target\debug\deps\weaver_proc_macro-b65c1ac59e89580a.d: src\lib.rs Cargo.toml
+
+src\lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
