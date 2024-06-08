34d9e7498278cf40f11e11b492e9edcdc1e6b025
clstatham <58794204+clstatham@users.noreply.github.com>
2024-06-08 14:38:26 -0500

    removed weaver-proc-macro crate (unused)

--- crates/weaver-proc-macro/target/debug/deps/weaver_proc_macro-6da2f8ce38a29c95.d
+++
@@ -1,9 +0,0 @@
-C:\Users\slash\code\weaver\weaver-proc-macro\target\debug\deps\weaver_proc_macro-6da2f8ce38a29c95.rmeta: src\lib.rs Cargo.toml
-
-C:\Users\slash\code\weaver\weaver-proc-macro\target\debug\deps\weaver_proc_macro-6da2f8ce38a29c95.d: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
